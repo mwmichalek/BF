34fa6167aeefe1245c0375dea628ba54e9c45b15
Michalek, Mark <michalek@21cf.com>
2019-11-21 09:25:31 -0500

    Charge is working well with multiple phases.

--- src/BF.Server/Components/BFComponent.cs
+++ src/BF.Server/Components/BFComponent.cs
@@ -18,25 +18,25 @@ namespace BF.Server.Components {
         protected IBeerFactoryEventHandler EventHandler { get; set; }
 
         [Inject]
-        protected ILoggerFactory _loggerFactory { get; set; }
+        protected ILoggerFactory LoggerFactory { get; set; }
 
-        protected ILogger _logger;
+        protected ILogger Logger;
 
-        private bool _isInitialized = false;
+        protected bool IsInitialized = false;
 
         protected override async Task OnInitializedAsync() {
-            //_logger = _loggerFactory.CreateLogger("SetPointComponent");
+
         }
 
         protected override void OnParametersSet() {
-            if (_isInitialized)
+            if (IsInitialized)
                 Initialize();
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender) {
             if (firstRender) {
                 Initialize();
-                _isInitialized = true;
+                IsInitialized = true;
             }
         }
 
